8c30cfa72621eccf35de207db7204886597b67c0
ZWF <13760141710@163.com>
2024-04-02 16:42:14 +0800

    优化：不允许点击跳转到当前步

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+// 1.仅针对当前着手，显示“You are at move #…”而不是按钮。
+// 2.重写 Board 以使用两个循环来制作方块而不是对它们进行硬编码。
+// 3.添加一个切换按钮，使可以按升序或降序对落子的步数进行排序。
+// 4.当有人获胜时，突出显示致使获胜的三个方块（当没有人获胜时，显示一条关于结果为平局的消息）。
+// 5.在“落子”的历史列表中以 (row, col) 格式显示每步的位置。
+
 export default function Game() {
   const [currentMove, setCurrentMove] = useState(0);
   const xisNext = currentMove % 2 === 0;
@@ -25,7 +31,12 @@ export default function Game() {
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {
+          move === currentMove ?
+            <strong>{`You are at move #${currentMove}`}</strong>
+            : (<button onClick={() => jumpTo(move)}>{description}</button>
+            )
+        }
       </li>
     )
   })
